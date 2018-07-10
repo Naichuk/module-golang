7e9dba81446840a8a3ef7c96f4701a6de56fc1a2
Marakhovskiy Ivan <anderhil1998@gmail.com>
2018-07-10 14:51:11 +0300

    Update downcase.go

--- ex1/downcase.go
+++ ex1/downcase.go
@@ -6,7 +6,7 @@ func Downcase(input string) (string, error) {
 	alph := map[string]string {
 		"A": "a", "B": "b", "C": "c", "D": "d", "E": "e", "F": "f", "G": "g", "H": "h",
 		"I": "i", "J": "j", "K": "k", "L": "l", "M": "m", "N": "n", "O": "o", "P": "p",
-		"Q": "q", "R": "r", "S": "s", "T": "t", "U": "u", "V": "v", "W": "w","X": "x",
+		"Q": "q", "R": "r", "S": "s", "T": "t", "U": "u", "V": "v", "W": "w", "X": "x",
 		"Y": "y", "Z": "z",
 	}
 	for i := 0; i < len(input); i++ {
