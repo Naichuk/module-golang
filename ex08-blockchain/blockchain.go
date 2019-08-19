673af749b0db902d984a0b00240ff5d952bfbe1b
Anna Kulakova <annkulakova7@gmail.com>
2019-08-19 21:50:55 +0300

    [WIP]: blockchain

--- ex08-blockchain/blockchain.go
+++ ex08-blockchain/blockchain.go
@@ -1,14 +1,14 @@
-package main
+/*package main
 
 import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"os"
 	"strconv"
 	"time"
-	"os"
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/mattn/go-sqlite3"
 )
 
 type Block struct {
@@ -26,7 +26,7 @@ func (b *Block) SetHash() {
 	b.Hash = hash[:]
 }
 
-func NewBlock(data string, prevBlockHash []byte) *Block i{ //creating a new block
+func NewBlock(data string, prevBlockHash []byte) *Block { //creating a new block
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
 	block.SetHash()
 	return block
@@ -51,42 +51,88 @@ func NewBlockchain() *Blockchain {
 }
 
 
-CREATE TABLE products(
-  id    INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, 
-  model  TEXT,
-  company TEXT,
-  price INTEGER
-)
+
+type Item struct {
+
+	Id int
+	Data string
+	Hash []byte
+	prevHash []byte
+}
 
 
-func main() {
-	db, err:= sql.Open("sqlite3", "./blockchain.db")
-	if err != nil {
-        panic(err)
-    }
-	defer db.Close()
-	switch os.Args[1] {
-	case "add"
-	result, err := db.Exec("insert into products (product, price )values (os.Args[2], os.Args[3])",
-    if err != nil{
-        panic(err)
-    	}
+
+func InitDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil { panic(err) }
+	if db == nil { panic("db nil") }
+	return db
+}
+
+
+func CreateTable(db *sql.DB) {
+	sql_table := `
+	CREATE TABLE IF NOT EXISTS block(
+		Id INTEGER PRIMARY KEY,
+		timestamp INTEGER,
+		data BLOB,
+		hash TEXT
+		prevHash TEXT
+	);
+	`
+	_, err := db.Exec(sql_table)
+	if err != nil { panic(err) }
+}
+
+
+
+func StoreItem(db *sql.DB, items []Item) {
+	sql_additem := `
+	INSERT OR REPLACE INTO items(
+		Id,
+		timestamp
+		data,
+		hash,
+		prevHash
+	) values(?, CURRENT_TIMESTAMP, ?, ?, ?)
+	`
+
+	stmt, err := db.Prepare(sql_additem)
+	if err != nil { panic(err) }
+	defer stmt.Close()
+
+	for _, item := range items {
+		_, err2 := stmt.Exec(item.Id, item.Name, item.Phone)
+		if err2 != nil { panic(err2) }
 	}
 }
 
 
-	/*bc := NewBlockchain()
-
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
 
 
 
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
+func main() {
+		db, err:= sql.Open("sqlite3", "./blockchain.db")
+		if err != nil {
+		    panic(err)
+		}
+		defer db.Close()
+		result, err := db.Exec("insert into products (product, price )values (os.Args[2], os.Args[3])",
+		if err != nil{
+		    panic(err)
+		    }
+		}
+		bc := NewBlockchain()
+		switch os.Args[1] {
+		case "add":
+			bc.AddBlock(os.Args[2])
+			fmt.Println("Transaction was added to the blockchain")
+		case "list":
+		for _, block := range bc.blocks {
+			fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+			fmt.Printf("Data: %s\n", block.Data)
+			fmt.Printf("Hash: %x\n", block.Hash)
+			fmt.Println()
+		}
+	}
 	}*/
-}
